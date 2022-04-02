36da88ddb5947b3b18310ef5bd9c0b48445ab35c
tejas <tejasdhonsale@gmail.com>
2022-04-02 15:28:05 +0530

    first

---
+++ android/app/src/main/kotlin/com/example/abc1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.abc1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:abc1/otp.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(new MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home':(context)=>HomeScreen(),
+        'otp':(context)=>MyOtp(),
+      },
+
+    );
+  }
+}
+
+
+
